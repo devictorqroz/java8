638a48a1ef15ffb1a14b3332b8d992fbd18f48b4
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-06-20 13:17:53 -0300

    first commit

---
+++ src/main/java/com/devictor/java8interfaces/Java8interfacesApplication.java
@@ -0,0 +1,12 @@
+package com.devictor.java8interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8interfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8interfacesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devictor/java8interfaces/Java8interfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devictor.java8interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8interfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
