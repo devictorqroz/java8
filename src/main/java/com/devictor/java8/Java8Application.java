ed80b11ac766527a7068ffa170943a3dca1c02bc
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-06-20 14:10:11 -0300

    ref: Rename project

--- src/main/java/com/devictor/java8/Java8Application.java
+++ src/main/java/com/devictor/java8/Java8Application.java
@@ -1,12 +1,12 @@
-package com.devictor.java8interfaces;
+package com.devictor.java8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java8interfacesApplication {
+public class Java8Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Java8interfacesApplication.class, args);
+		SpringApplication.run(Java8Application.class, args);
 	}
 }
--- src/test/java/com/devictor/java8/Java8ApplicationTests.java
+++ src/test/java/com/devictor/java8/Java8ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devictor.java8interfaces;
+package com.devictor.java8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Java8interfacesApplicationTests {
+class Java8ApplicationTests {
 
 	@Test
 	void contextLoads() {
