ede11bb130aa8e92e88ae0070697a73c4ea77709
Victor Queiroz <victorqueiroz.ti@gmail.com>
2024-06-24 18:15:59 -0300

    Synchronized e Collections

---
+++ src/main/java/com/devictor/java8/multithread/SincronizarColecoes.java
@@ -0,0 +1,43 @@
+package com.devictor.java8.multithread;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SincronizarColecoes {
+
+    private static List<String> lista = new ArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Utilize a versão do synchronized de acordo com a Collection
+        lista = Collections.synchronizedList(lista);
+
+        MeuRunnable runnable = new MeuRunnable();
+
+        Thread t0 = new Thread(runnable);
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+
+        t0.start();
+        t1.start();
+        t2.start();
+
+        Thread.sleep(500);
+
+        System.out.println(lista);
+
+    }
+
+    public static class MeuRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            lista.add("#");
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " inseriu");
+        }
+
+    }
+
+}
